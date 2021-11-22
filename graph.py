8a85573a3a48b24feddb8fe9c351854ffe31531b
rebekaraffay <rebekaraffay@gmail.com>
2021-11-22 12:59:40 +0100

    is_parent

--- graph.py
+++ graph.py
@@ -3,7 +3,7 @@ import time
 import numpy as np
 from node import Node
 from state import State
-import itertools as it
+#import itertools as it
 from itertools import permutations
 
 
@@ -42,11 +42,44 @@ class Graph:
 
 
 
-    #todo: def is_sibling   # szintek között megfelelő élek behúzásához, hogy 1-e a különbség
+    #todo: def is_parent   # szintek között megfelelő élek behúzásához, hogy 1-e a különbség
     #todo: irányított élek behúzása szintek között (kiv ha az ős nyerő), 2x végig káne menni,
     #           hogy gyereke-e, azt eltárolni a nodeban, majd, 2. körben, hogy őse-e és azt is node-ban eltárolni
     #todo: kiszedni a nem kapcsolódó dolgokat (amik ősei nyertesek) -> nem teszem bele őket a következő tömbbe
     #todo: nyerő, vesztő mezők kigyűjtése
+
+    def is_parent(self, last_level, current_level):
+        # Count occurrence of element '1 and 0' in each row and from that say if one is a parent of the other
+        index_last = self.levels.index(last_level)  # how many elements are different from -1 (not empty)
+        index_current = self.levels.index(current_level)
+        if index_last == index_current - 1:
+            indices_last = []
+            indices_current = []
+            parent = []
+            for i in range(len(last_level)):
+                count1 = np.count_nonzero(last_level[i] == 1, axis=1)  # how many elements are 1 in a matrix
+                count0 = index_last-count1
+                indices_last.append([last_level[i], count1, count0])
+            for i in range(len(current_level)):
+                count1 = np.count_nonzero(current_level[i] == 1, axis=1)
+                count0 = index_current-count1
+                indices_current.append([current_level[i], count1, count0])
+            for i in range(len(indices_last)):
+                for j in range(len(indices_current)):
+                    if indices_last[i][1] == indices_current[j][1] and indices_last[i][2] == indices_current[j][2]-1:
+                        parent.append((indices_last[i][0], indices_current[j][0]))
+                    elif indices_last[i][1] == indices_current[j][1]-1 and indices_last[i][2] == indices_current[j][2]:
+                        parent.append((indices_last[i][0], indices_current[j][0]))
+            return parent
+
+
+
+
+
+
+
+
+
 
 
 
