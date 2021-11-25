152ffad9a9b3b5ea75528dbac198b0eeba8554ba
rebekaraffay <rebekaraffay@gmail.com>
2021-11-25 23:09:49 +0100

    Json proba

--- strategy.py
+++ strategy.py
@@ -1,3 +1,5 @@
+import json
+
 # todo: open dictionaris
 # csak pl, hogy legyen mivel dolgoznod
 d = {}  # üres szótár, ekvivalens: d = dict()
@@ -5,3 +7,12 @@ d["apple"] = 12
 d["plum"] = 2
 d[1]=4
 d
+
+#if __name__ == "__main__":
+    #with open("proba_szoveg_kiiras.json", "w") as write_file:
+        #json.dump(d, write_file)
+
+if __name__ == "__main__":
+    with open("proba_szoveg_kiiras.json", "r") as read_file:
+        data = json.load(read_file)
+        print(data.keys())
