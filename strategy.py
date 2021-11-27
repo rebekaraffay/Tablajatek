58fbc2ca7c828ee2107e0dc56dd8deffaafe3346
rebekaraffay <rebekaraffay@gmail.com>
2021-11-27 18:37:05 +0100

    json
    proba = nem stringesitett ertekekkel a save_datas() dictionaryk
    teljes_szulo_gyerek_szotarak = stringesitve ugyanez
    proba_seta_teljes = 2 ertek minden csucshoz, utak szama
    nyero/veszto lepesek = elozo commitomban a hosszabb kommentben van

--- strategy.py
+++ strategy.py
@@ -1,16 +1,10 @@
 import json
 import graph
 
-# if __name__ == "__main__":
-  #  with open("proba.json", "r") as read_file:
-   #     data = json.load(read_file)
-    #    print(len(data[0].keys()))
-     #   print(len(data[1].keys()))
-
-if __name__ == "__main__":
-    q = graph.Graph().save_datas()
-    with open("teljes_szulo_gyerek_szotarak.json", "w") as write_file:
-        json.dump(q, write_file)
+#if __name__ == "__main__":
+ #   q = graph.Graph().save_datas()
+  #  with open("teljes_szulo_gyerek_szotarak.json", "w") as write_file:
+   #     json.dump(q, write_file)
 
 #if __name__ == "__main__":
     # p = graph.Graph().save_datas()
