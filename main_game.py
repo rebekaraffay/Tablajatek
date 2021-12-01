c110677c2816f74fb04730e1bb729fb9d8cc4608
rebekaraffay <rebekaraffay@gmail.com>
2021-12-01 17:20:18 +0100

    main_game es a check a computer playben is jo

--- main_game.py
+++ main_game.py
@@ -1,6 +1,16 @@
-# todo: import kb minden
-# todo: válasszon, hogy multi, vagy géppel, ha géppel akkor melyik eset
-# todo: a megfelelő fusson le
-# todo: annak a működésének a megírása, amikor a gép a szótárak alapján lép
 # todo: játékszabélyok leírása világosan az elején
 # todo: readme-be dokumentáció valami minimális szinten
+
+import computer_play as cp
+import multiplayer as mp
+
+def special_amoba():
+    tipus = int(input("Egyedul vagy egy baratoddal szeretnel jatszani? Ha egyedul: 1, ha nem: 2: "))
+    if tipus == 1:
+        cp.final_game()
+    if tipus == 2:
+        mp.multiplayer()
+
+
+if __name__ == "__main__":
+    special_amoba()
